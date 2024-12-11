637b7c0596640617e546c315ebf90bccf38b9186
Ronak Nisher <nisher.ronak@gmail.com>
2024-12-11 16:38:42 +0530

    day 11 improvements

--- src/day11.rs
+++ src/day11.rs
@@ -5,19 +5,17 @@ pub fn solve(input: String) {
     let mut result_part2 = 0;
 
     let bricks: Vec<u64> = input.split(" ").map(|x| x.parse().unwrap()).collect();
-    let mut bricks_map: HashMap<u64, u64> = bricks.iter().map(|x| (*x, 1)).collect();
+    let mut bricks_map: HashMap<u64, u64> = HashMap::new();
+    bricks.iter().for_each(|&x| { *bricks_map.entry(x).or_insert(0) += 1; });
 
     let num_blinks_part1 = 25;
     let num_blinks_part2 = 75;
     for i in 0..num_blinks_part2 {
-        let mut new_bricks: Vec<u64> = Vec::new();
         let mut new_bricks_map: HashMap<u64, u64> = HashMap::new();
-        new_bricks.reserve(bricks.len() * 2);
 
         for (brick, count) in bricks_map.iter() {
             if brick == &0 {
-                let new_val = new_bricks_map.get(&1).unwrap_or(&0) + 1 * count;
-                new_bricks_map.insert(1, new_val);
+                *new_bricks_map.entry(1).or_insert(0) += count;
                 continue;
             }
 
@@ -29,22 +27,22 @@ pub fn solve(input: String) {
                 let left_digit = left.parse::<u64>().unwrap();
                 let right_digit = right.parse::<u64>().unwrap();
 
-                new_bricks_map.insert(left_digit, new_bricks_map.get(&left_digit).unwrap_or(&0) + 1 * count);
-                new_bricks_map.insert(right_digit, new_bricks_map.get(&right_digit).unwrap_or(&0) + 1 * count);
+                *new_bricks_map.entry(left_digit).or_insert(0) += count;
+                *new_bricks_map.entry(right_digit).or_insert(0) += count;
                 continue;
             }
 
-            new_bricks_map.insert(brick * 2024, new_bricks_map.get(&(brick * 2024)).unwrap_or(&0) + 1 * count);
+            *new_bricks_map.entry(brick * 2024).or_insert(0) += count;
         }
 
-        bricks_map = new_bricks_map.clone();
+        bricks_map = new_bricks_map;
 
         if i == num_blinks_part1 - 1 {
-            result = new_bricks_map.values().sum();
+            result = bricks_map.values().sum();
         }
 
         if i == num_blinks_part2 - 1 {
-            result_part2 = new_bricks_map.values().sum();
+            result_part2 = bricks_map.values().sum();
         }
     }
 
